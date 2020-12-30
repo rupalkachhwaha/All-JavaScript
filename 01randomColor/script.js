3e9a29e98fe2ea0e1fcbfaef2849178361b19f68
rupal kachhwaha <35149577+rupalkachhwaha@users.noreply.github.com>
2020-12-31 01:17:02 +0530

    Update script.js

--- 01randomColor/script.js
+++ 01randomColor/script.js
@@ -1,13 +1,33 @@
-        let button = document.querySelector('#buttonId');
-        let body = document.querySelector('body');
 
-        let colors = ['aqua', 'violet', 'lightGreen', 'pink', 'DarkOrange','silver'];
-
-        button.style.backgroundColor = 'violet';
-        button.addEventListener('click', changeBackground);
-
-        function changeBackground() {
-                let colorIndex = Math.ceil(Math.random() * colors.length);
+        // Hex Change Background Color 
+        const button = document.querySelector('#buttonId')
+        const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+        
+        button.style.backgroundColor = '#c1c1c1';
+        button.addEventListener('click', () => {
+            let hex = '#'
+    
+            for (let i = 0; i < 6; i++){
+                const index = Math.floor(Math.random()*hexValues.length)
                 
-                button.style.backgroundColor= colors[colorIndex];
-        }
+                hex = hex + hexValues[index]
+                
+            }
+            
+            button.style.backgroundColor = hex
+
+        } )
+    
+        
+     
+        //Change Background Color Project
+
+        // let button = document.querySelector('#buttonId');
+        // button.style.backgroundColor = 'red';
+        // let colors = ['aqua', 'violet', 'lightGreen', 'pink', 'DarkOrange','silver'];
+
+        // button.addEventListener('click' , () => {
+        //         colorIndex= Math.floor(Math.random() * colors.length);
+        //         button.style.backgroundColor = colors[colorIndex];
+        // })
+
